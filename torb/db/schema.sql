5d88eedf243a99ae3fcb4be5a5e63f5712bd2690
sakamoto-shun <sakamoto-shun@kayac.com>
2018-09-15 12:58:03 +0900

    reservationsのuser_idにindexを貼る

--- torb/db/schema.sql
+++ torb/db/schema.sql
@@ -31,6 +31,7 @@ CREATE TABLE IF NOT EXISTS reservations (
     reserved_at DATETIME(6)      NOT NULL,
     canceled_at DATETIME(6)      DEFAULT NULL,
     KEY event_id_and_sheet_id_idx (event_id, sheet_id)
+    KEY user_id_idx (user_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS administrators (
