813c97d51cb9bf55fe373e915173fb61372c21e4
sakamoto-shun <sakamoto-shun@kayac.com>
2018-09-15 13:53:32 +0900

    カンマが足りてない

--- torb/db/schema.sql
+++ torb/db/schema.sql
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS reservations (
     reserved_at DATETIME(6)      NOT NULL,
     canceled_at DATETIME(6)      DEFAULT NULL,
     KEY event_id_and_sheet_id_idx (event_id, sheet_id),
-    KEY user_id_idx (user_id)
+    KEY user_id_idx (user_id),
     KEY reserved_at_idx (reserved_at)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
