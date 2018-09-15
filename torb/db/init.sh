058301d90ab7f3305bcde75154469619d7ce18bf
Suzuki Kyosuke <suzuki-kyosuke@kayac.com>
2018-09-15 15:28:24 +0900

    slowlogをrotateする

--- torb/db/init.sh
+++ torb/db/init.sh
@@ -4,6 +4,8 @@ ROOT_DIR=$(cd $(dirname $0)/..; pwd)
 DB_DIR="$ROOT_DIR/db"
 BENCH_DIR="$ROOT_DIR/bench"
 
+sudo mv /var/log/mariadb/slow.log /var/log/mariadb/slow.log.$(date +%H%M)
+
 export MYSQL_PWD=isucon
 export MYSQL_HOST="127.0.0.1"
 
@@ -19,3 +21,5 @@ fi
 mysql -uisucon torb -e 'ALTER TABLE reservations DROP KEY event_id_and_sheet_id_idx'
 gzip -dc "$DB_DIR/isucon8q-initial-dataset.sql.gz" | mysql -uisucon torb
 mysql -uisucon torb -e 'ALTER TABLE reservations ADD KEY event_id_and_sheet_id_idx (event_id, sheet_id)'
+
+mysql -uisucon torb -e 'flush slow logs'
